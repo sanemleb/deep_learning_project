82737806eb510882aaf6bd80e372c4199b16105e
Sanem Leblebici <sanemleblebici@gmail.com>
2023-11-18 00:00:01 +0100

    something works

--- colab_dependencies/model.py
+++ colab_dependencies/model.py
@@ -65,4 +65,7 @@ class UNET(nn.Module):
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
 
+        # Convert the final output to the same data type as the input
+        x = x.to(dtype=x.dtype)
+
         return self.final_conv(x)
