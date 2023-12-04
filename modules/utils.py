48d1fb73571abbf309d935a5e61280d41ab0b0ca
micak <michallehwark511@gmail.com>
2023-12-04 13:54:10 +0100

    new dataset generation methods, focal loss

--- modules/utils.py
+++ modules/utils.py
@@ -1,7 +1,8 @@
 from torch.utils.data import random_split, DataLoader
 from modules.dataset import CarDataset
 import numpy as np
-import torchvision.transforms.functional as F
+import torch.nn.functional as F
+import torch.nn as nn
 import torch
 
 def get_data_loaders(data_path, batch_size, split_ratio=0.8):
@@ -38,6 +39,35 @@ class AddGaussianNoise(object):
         noisy_pil_image = F.to_pil_image(noisy_tensor)
 
         return noisy_pil_image
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2, weight=None, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.weight = weight
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            input = input.view(input.size(0), input.size(1), -1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))  # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = torch.exp(logpt)
+
+        if self.weight is not None:
+            pt = pt * self.weight.gather(0, target.view(-1))
+
+        loss = -((1 - pt) ** self.gamma) * logpt
+
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
 
 # Evaluation functions
 
